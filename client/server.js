ef11b04ba04aafba47edc86c870ffafcc43db715
Ad <ad@izit.be>
2015-05-20 20:47:09 +0200

    te

--- client/server.js
+++ client/server.js
@@ -9,16 +9,17 @@ var path = require('path');
 var app = express();
 
 // Configuration Express server
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-app.set('view option', {
-	layout: false
-});
+//app.set('view engine', 'jade');
+//app.set('views', __dirname + '/views');
+//app.set('view option', {
+//	layout: false
+//});
 app.use(express.static(__dirname + '/'));
 
 // Routes
 app.get('/', function(req, res) {
-	res.render('index');
+	//res.render('index');
+	res.sendFile(path.join(__dirname + '/views/transactions.html'));
 });
 
 var server = app.listen(3001, function() {
